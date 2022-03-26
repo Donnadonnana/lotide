25246bcbd025c4884a8d3b0edb7100fed5d4f87a
donnahuang <donnaphzj@gmail.com>
2022-03-25 20:21:56 -0400

    -m"changes"

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function (actual, expected) {
   if (actual !== expected) {
-    console.log("Assertion failed")
+    console.log(`Assertion failed`)
   } else {
-    console.log("Assertion Passed")
+    console.log(`Assertion Passed`)
   }
 };
 
