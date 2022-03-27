226e3860100cf474db48c83770692686d27ee5f2
donnahuang <donnaphzj@gmail.com>
2022-03-27 11:55:41 -0400

    assert arrays equal

---
+++ assertArraysEqual.js
@@ -0,0 +1,28 @@
+const assertEqual = function (actual, expected) {
+  if (actual !== expected) {
+    console.log(`Assertion failed`)
+  } else {
+    console.log(`Assertion Passed`)
+  }
+};
+
+const eqArrays = function (arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  }
+  for (var i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const assertArrayEqual = function (array1, array2) {
+  if (eqArrays(array1, array2) === false) {
+    console.log(`Assertion failed`)
+  } else {
+    console.log(`Assertion Passed`)
+  }
+};
+
