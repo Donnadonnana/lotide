922ad910ae0c443da695c97b8422758ab2dd87b6
donnahuang <donnaphzj@gmail.com>
2022-03-27 13:30:28 -0400

    middle

---
+++ middle.js
@@ -0,0 +1,40 @@
+//assertArrayEqual and eqArrays function
+const eqArrays = function (arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  }
+  for (var i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const assertArraysEqual = function (array1, array2) {
+  if (eqArrays(array1, array2) === false) {
+    console.log(`Assertion failed`)
+  } else {
+    console.log(`Assertion Passed`)
+  }
+};
+
+const middle = function (array) {
+  let middleArray = [];
+
+  if (array.length === 1 || array.length === 2) return [];
+
+  const isOdd = array.length % 2 === 1;
+  if (isOdd) {
+    const middleIndex = ((array.length + 1) / 2) - 1;
+    middleArray.push(array[middleIndex])
+  } else {
+    const middleIndexLower = array.length / 2 - 1;
+    const middleIndexHigher = middleIndexLower + 1;
+    middleArray.push(array[middleIndexLower]);
+    middleArray.push(array[middleIndexHigher]);
+  }
+  return middleArray;
+}
+
+console.log(middle([1, 2, 3, 4, 5, 6]));
