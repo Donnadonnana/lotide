7c3a9be2e08823aeafddfcd2b9fa5aaaf29485b8
donnahuang <donnaphzj@gmail.com>
2022-03-30 14:27:39 -0400

    letter position object

---
+++ letterPositions.js
@@ -0,0 +1,43 @@
+const assertEqual = function (actual, expected) {
+  if (actual !== expected) {
+    console.log(`Assertion failed`)
+  } else {
+    console.log(`Assertion Passed`)
+  }
+};
+
+const eqArrays = function (arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  }
+  for (var i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const assertArrayEqual = function (array1, array2) {
+  if (eqArrays(array1, array2) === false) {
+    console.log(`Assertion failed`)
+  } else {
+    console.log(`Assertion Passed`)
+  }
+};
+
+const letterPositions = function (sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (results[sentence[i]]) {
+      results[sentence[i]] += [i];
+    } else {
+      results[sentence[i]] = [i];
+    }
+  }
+  return results;
+};
+
+
+console.log(letterPositions('lighthouse').h)
+assertArrayEqual(letterPositions("hello").e, [1]);
